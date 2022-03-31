365ea68a52a258d13f59769bef65abb51298b09b
Nischey Gupta <ngupta16@my.bcit.ca>
2022-04-01 04:58:43 +0530

    updated the file to save the bookmarks for the user on firebase

--- scripts/browsePosts.js
+++ scripts/browsePosts.js
@@ -53,6 +53,14 @@ function displayCards(collection) {
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-text').innerHTML = details;
                 // newcard.querySelector('.card-text').innerHTML = province;
+                // newcard.querySelector('a').onclick = () => setAccommodationsData(code);
+
+                //this line sets the id attribute for the <i> tag in the format of "save-code" 
+                //so later we know which hike to bookmark based on which hike was clicked
+                newcard.querySelector('i').id = 'save-' + code;
+                // this line will call a function to save the hikes to the user's document
+                newcard.querySelector('i').onclick = () => saveBookmark(code);
+
                 newcard.querySelector('.card-image').src = "./images/" + code + ".jpg"; //accommodations.jpg
 
                 //give unique ids to all elements for future use
@@ -68,3 +76,39 @@ function displayCards(collection) {
 }
 
 displayCards("accommodations");
+
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);   //global
+        console.log(currentUser);
+
+        // the following functions are always called when someone is logged in
+        read_display_Quote();
+        insertName();
+        populateCardsDynamically();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+
+//-----------------------------------------------------------------------------
+// This function is called whenever the user clicks on the "bookmark" icon.
+// It adds the accommodation to the "bookmarks" array
+// Then it will change the bookmark icon from the hollow to the solid version. 
+//-----------------------------------------------------------------------------
+function saveBookmark(code) {
+    currentUser.set({
+            bookmarks: firebase.firestore.FieldValue.arrayUnion(code)
+        }, {
+            merge: true
+        })
+        .then(function () {
+            console.log("bookmark has been saved for: " + currentUser);
+            var iconID = 'save-' + code;
+            //console.log(iconID);
+            document.getElementById(iconID).innerText = 'bookmark';
+        });
+}

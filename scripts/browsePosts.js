a9687b2de566171b84da800985ab87408eff2ed3
Nischey Gupta <ngupta16@my.bcit.ca>
2022-04-06 22:47:37 +0530

    commented unnecessary code

--- scripts/browsePosts.js
+++ scripts/browsePosts.js
@@ -86,9 +86,9 @@ firebase.auth().onAuthStateChanged(user => {
         console.log(currentUser);
 
         // the following functions are always called when someone is logged in
-        read_display_Quote();
-        insertName();
-        populateCardsDynamically();
+        // read_display_Quote();
+        // insertName();
+        // populateCardsDynamically();
     } else {
         // No user is signed in.
         console.log("No user is signed in");

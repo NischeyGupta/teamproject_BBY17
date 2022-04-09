8bfae6ada83a3f3af75ed091c9d087c6cf6a06fe
Nischey Gupta <ngupta16@my.bcit.ca>
2022-04-09 09:40:43 +0530

    updated the saved.html page to show the bookmarked accommodations of the user

--- scripts/saved.js
+++ scripts/saved.js
@@ -20,11 +20,11 @@ function getBookmarks(user) {
                     size = snap.size;
                     queryData = snap.docs;
 
-                    if (size = 1) {
+                    if (size == 1) {
                         var doc = queryData[0].data();
-                        var title = doc.data().name; // get value of the "name" key
-                        var details = doc.data().details; // get value of the "details" key
-                        var code = doc.data().code; // get value of the "details" key
+                        var title = doc.name; // gets the name fiels
+                        var details = doc.details; // get the details
+                        var code = doc.code; // gets the code
                         // var province = doc.data().province; // get value of the "province" key
                         let newcard = accommodationCardTemplate.content.cloneNode(true);
 
@@ -35,46 +35,9 @@ function getBookmarks(user) {
 
                         newcard.querySelector('.card-image').src = "./images/" + code + ".jpg"; //accommodations.jpg
 
-                        document.getElementById(collection + "-go-here").appendChild(newcard);
-                        i++;
-                    } else {
-                        console.log("Query has more than one data")
-                    }
 
-                })
-
-            });
-        })
-}
-
-
-function getBookmarks(user) {
-    db.collection("users").doc(user.uid).get()
-        .then(userDoc => {
-            var bookmarks = userDoc.data().bookmarks;
-            console.log(bookmarks);
-
-            let CardTemplate = document.getElementById("accommodationCardTemplate");
-
-            bookmarks.forEach(code => {
-                console.log(code);
-                db.collection("accommodations").where("code", "==", code).get().then(snap => {
-                    size = snap.size;
-                    queryData = snap.docs;
-                    // console.log(bookmarks);
-                    if (size != 1) {
-                        var doc = queryData[0].data();
-                        var title = doc.data().name; // get value of the "name" key
-                        var details = doc.data().details; // get value of the "details" key
-                        // var code = doc.data().code; // get value of the "details" key
-                        let newCard = CardTemplate.content.cloneNode(true);
-                        newCard.querySelector('.card-title').innerHTML = title;
-                        newCard.querySelector('.card-text').innerHTML = details;
-                        newCard.querySelector('a').onclick = () => setAccommodationsData(code);
-                        newCard.querySelector('.card-image').src = "./images/" + code + ".jpg"; //accommodations.jpg
-
-
-                        document.getElementById(collection + "-go-here").appendChild(newCard);
+                        accommodationCardGroup.appendChild(newcard);
+                        // document.getElementById(collection + "-go-here").appendChild(newcard);
                     } else {
                         console.log("Query has more than one data")
                     }
